3390796c737cdd6c06915622a676e0599b9268a6
samminmul <74859160+samminmul@users.noreply.github.com>
2025-09-17 10:02:09 +0000

    week1

--- week1/src/index.js
+++ week1/src/index.js
@@ -1,1 +1,12 @@
+const express = require('express');
 
+const app = express();
+
+app.get('/hello', (req, res) => { 
+    let i = 2 * 2;
+    res.send(i); 
+});
+
+app.get('/hi', (req, res) => { res.send('<h1>안녕2<h1>'); });
+
+app.listen(3000, () => {console.log('Server listening on port 3000!')});
