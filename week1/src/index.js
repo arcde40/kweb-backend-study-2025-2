6c26474dfc07d8833c8f383f581ac2eeec18b9d9
ggongch2 <111619666+ggongch2@users.noreply.github.com>
2025-09-17 10:02:09 +0000

    week1

--- week1/src/index.js
+++ week1/src/index.js
@@ -1,1 +1,17 @@
+const express = require('express') ;
+
+const app = express() ;
+
+app.get('/hello', (req, res) =>{
+    let i = 2 * 2 ;
+    res.send('하이!!!!!!!!') ;  
+    res.send(i);  
+}) ;
+
+
+app.get('/hi', (req, res) =>{
+    res.send('잘가!!!!!!!!') ;  
+}) ;
+
+app.listen(3000, () => {console.log('port 3000 HI !!!!!!!!!!') ; }) ;
 
