d92315bbe39198fbbc6504adc6972008cccdcbd9
ChanHaeng Lee <33365191+Lockway@users.noreply.github.com>
2025-09-17 10:02:14 +0000

    create index.js

--- week1/src/index.js
+++ week1/src/index.js
@@ -1,1 +1,15 @@
+const express = require('express')
+const app = express();
+const PORT = 3000;
 
+app.get('/hello', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.get('/bye', (req, res) => {
+    res.send('Goodbye world!');
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running');
+});
