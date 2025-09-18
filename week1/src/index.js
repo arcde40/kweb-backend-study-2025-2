423a7679a441ad781e638a785ebfa41a1864a2cd
AHN, Seongyeon <102126594+seongyeon313@users.noreply.github.com>
2025-09-18 06:13:40 +0000

    week1

--- week1/src/index.js
+++ week1/src/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/hello', (req, res) => {res.send('Hello, World!');});
 
-app.get('/bye', (req, res) => {res.send('Bye, World!');});
+app.get('/bye', (req, res) => {res.send('Goodbye, World!');});
 
 app.listen(3000, () => {console.log('Server listening on port 3000')});
