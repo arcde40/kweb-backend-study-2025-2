04e16c4dbc12969fadd7f6ec32c3eb7b4287bb46
seongyoon3844 <66810059+zeroplam5@users.noreply.github.com>
2025-09-21 08:21:05 +0000

    첫째 과제

--- week1/src/index.js
+++ week1/src/index.js
@@ -1,1 +1,10 @@
+const express = require('express')
 
+const app = express()
+
+app.get('/hello', (req, res) => { res.send('<h1>Hello World!</h1>')})
+
+app.get('/bye', (req, res) => { res.send('<h1>Goodbye World!</h1>')})
+
+
+app.listen(3000, () => {console.log('Server listening on port 3000')})
