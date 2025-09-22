96de1b4964e66d3f6051fbfe1a5be403d7edcbb8
inazawa0622 <mchang1492@gmail.com>
2025-09-22 01:49:49 +0000

    week1

--- week1/src/index.js
+++ week1/src/index.js
@@ -1,1 +1,5 @@
-
+const express = require('express');
+const app = express();
+app.get('/hello',(req,res)=>{res.send('Helllo, World!');});
+app.get('/bye',(req,res)=>{res.send('Goodbye, World!');});
+app.listen(3000,()=>{console.log('Serer listening on port 3000!')})
