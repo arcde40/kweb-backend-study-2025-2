bc4a082ca02c23a591ba22f8cb8c822872fd1984
eajnnim <kimmj7876@naver.com>
2025-09-24 02:11:57 +0900

    week1 hw

--- week1/src/index.js
+++ week1/src/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
-app.get('/hello', (req, res) => { 
-    let i = 2 * 2;
-    res.send(i); 
-});
-app.get('/hi', (req, res) => { res.send('잘가'); });
+app.get('/hello', (req, res) => { res.send('Hello, World!'); });
+app.get('/bye', (req, res) => { res.send('Goodbye, World!'); });
 // req 왔을때 수행할 동작 정의
 
 app.listen(3000, () => { console.log('Server listening on port 3000.')});
