3701e814afa262102e8bb23d495ca0036da56dc8
Koreangung15 <76540675+Koreangung15@users.noreply.github.com>
2025-09-24 15:37:09 +0000

    week2 과제 최종git status

--- week2/math.js
+++ week2/math.js
@@ -2,11 +2,44 @@ const { Router } = require('express')
 
 const router = Router();
 
+const HTMLtemplate = (text) => {
+    const template = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <style>
+                @font-face {
+                    font-family: 'Yangjin';
+                    src: url('https://cdn.jsdelivr.net/gh/supernovice-lab/font@0.9/yangjin.woff') format('woff');
+                    font-weight: normal;
+                    font-display: swap;
+                }
+                .my_text{
+                    font-family: 'Yangjin';
+                    font-size: 40px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class = "my_text">${text}</div>
+        </body>
+    </html>
+    `
+
+    return template
+}
+
 router.get('/', (req, res) => {
-    res.send(`안녕하세요, 현재 URL의 뒤에 "/add", "/sub" 혹은 "/mul" 을 붙여서 두 수의 연산 기능을 수행하실 수 있습니다. div는 귀찮아서 안했어옹.`)
+    res.send(HTMLtemplate(`안녕하세요, 현재 URL의 뒤에
+        <br>
+        "/add", "/sub" 혹은 "/mul" 을 붙여서 두 수의 연산 기능을 수행하실 수 있습니다.
+        <br>
+        div는 귀찮아서 안했어옹.`
+    ))
 })
 
 const operator = ["add", "sub", "mul"]
+
 
 operator.forEach((value, index) => {
     router.get(`/${value}`, (req, res) => {
@@ -14,17 +47,25 @@ operator.forEach((value, index) => {
         const b = Number(req.query.b)
 
         if(!a || !b){
-            res.send(`이런! 연산하고자 하는 값이 주어지지 않았습니다.\nURL의 "/${value}" 문자열 뒤에 "?a={숫자}&b={숫자}" 를 알맞게 넣어서 입력해주시길 바랍니다.`)
+            res.send(HTMLtemplate(`이런! 연산하고자 하는 값이 주어지지 않았습니다. 
+                <br> 
+                URL의 "/${value}" 문자열 뒤에 
+                <br>
+                <br>
+                "?a={숫자}&b={숫자}"
+                <br>
+                <br>
+                 를 알맞게 넣어서 입력해주시길 바랍니다.`))
         }
         else{
             if(value === "add"){
-                res.send(`결과 : ${a + b}`)
+                res.send(HTMLtemplate(`a + b = ${a + b}`))
             }
             else if(value === "sub"){
-                res.send(`결과 : ${a - b}`)
+                res.send(HTMLtemplate(`a - b = ${a - b}`))
             }
             else{
-                res.send(`결과 : ${a * b}`)
+                res.send(HTMLtemplate(`a * b = ${a * b}`))
             }
         }
     })
