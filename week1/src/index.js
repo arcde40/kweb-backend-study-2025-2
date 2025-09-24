9252ab6348470e8d61af26c8db3dfa11f110899f
jihoon0065 <jihoon0065@gmail.com>
2025-09-24 07:26:10 +0000

    week1

--- week1/src/index.js
+++ week1/src/index.js
@@ -1,1 +1,7 @@
+const express = require("express");
+const app = express();
 
+app.get('/',(req,res)=>{res.send('Hello,world!');});
+app.get('/bye',(req,res)=>{res.send('good bye...!');});
+
+app.listen(3000, ()=>{console.log('Server listening on Port 3000');});
