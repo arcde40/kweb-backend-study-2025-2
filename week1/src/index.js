3afe64bf3c416b8d09ffcced34446f5f38a0d905
eric0009 <zeroplam5@gmail.com>
2025-10-07 01:03:36 +0900

    2주차 과제

--- week1/src/index.js
+++ week1/src/index.js
@@ -1,1 +1,10 @@
+const express = require('express')
 
+const app = express()
+const port = 3000
+
+app.get(`/user/:id`, (req, res) => {
+    res.send(`Hello user ${req.params.id}`)
+})
+
+app.listen(3000, () => console.log(`Server running on port ${port}...`))
