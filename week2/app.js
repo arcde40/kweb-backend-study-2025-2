d0ca14344cc4d8848ea5fc2a79fc9e3854b97406
laughor123 <haminjoo06@gmail.com>
2025-10-10 16:30:57 +0000

    week2 practice

---
+++ week2/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const userRouter = require('./user');
+
+app.use((req ,res, next) => {
+    console.log(`Someone sent a request: ${req.url}`)
+    next();
+})
+
+app.use('/user', userRouter);
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(3000, () => console.log("Server listening on port 3000"));
+
