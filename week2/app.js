293c9305c1a6293f9744c70208d9f3dd86aa09a5
laughor123 <haminjoo06@gmail.com>
2025-10-10 17:01:50 +0000

    week2 practice and homework

--- week2/app.js
+++ week2/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 const userRouter = require('./user');
+const mathRouter = require('./math');
 
 app.use((req ,res, next) => {
     console.log(`Someone sent a request: ${req.url}`)
@@ -10,6 +11,7 @@ app.use((req ,res, next) => {
 })
 
 app.use('/user', userRouter);
+app.use('/math', mathRouter);
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
