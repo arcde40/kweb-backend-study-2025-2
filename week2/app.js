2f8461fc587139b66c788af1b4d1c68a2dfcccf3
pip_install_lup <133948307+ovrhit@users.noreply.github.com>
2025-11-11 07:00:19 +0000

    week3 practice

--- week2/app.js
+++ week2/app.js
@@ -3,9 +3,30 @@ const app = express();
 const userRouter = require('./user');
 const mathRouter = require('./math');
 
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
+app.use(express.urlencoded({extended : true}));
+app.use(express.static('public'));
+
 app.use((req, res, next) => {
     console.log(`Someone made a request to ${req.url}`);
     next();
+});
+
+app.get('/math/sum', (req, res) => {
+    res.render('mathsite', { 
+        title: 'Addition', 
+        action: '/math/sum', 
+        buttonLabel: 'Add Numbers' 
+    });
+});
+
+app.get('/math/sub', (req, res) => {
+    res.render('mathsite', { 
+        title: 'Subtraction', 
+        action: '/math/sub', 
+        buttonLabel: 'Subtract Numbers' 
+    });
 });
 
 app.use(userRouter);
--- week2/math.js
+++ week2/math.js
@@ -1,26 +1,47 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/sum', (req, res, next) => {
-    const a = parseInt(req.query.a);
-    const b = parseInt(req.query.b);
-    if (isNaN(a) || isNaN(b)) res.send(`Unvalid value!`);
-    else next();
-})
+router.get('/', (req, res) => res.redirect('/math/sum'));
 
-router.get('/sum', (req, res) => {
-    res.send(`${req.query.a} + ${req.query.b} = ${parseInt(req.query.a) + parseInt(req.query.b)}`);
-})
+router.post('/sum', (req, res, next) => {
+    const a = parseFloat(req.body.num1);
+    const b = parseFloat(req.body.num2);
+    if (isNaN(a) || isNaN(b)) {
+        res.send('Invalid value!');
+    } else {
+        req.result = a + b;
+        next();
+    }
+});
 
-router.get('/sub', (req, res, next) => {
-    const a = parseInt(req.query.a);
-    const b = parseInt(req.query.b);
-    if (isNaN(a) || isNaN(b)) res.send(`Unvalid value!`);
-    else next();
-})
+router.post('/sum', (req, res) => {
+    res.render('mathsite', { 
+        title: 'Addition',
+        action: '/sum',
+        buttonLabel: 'Add Numbers',
+        result: req.result
+    });
+});
 
-router.get('/sub', (req, res) => {
-    res.send(`${req.query.a} - ${req.query.b} = ${parseInt(req.query.a) - parseInt(req.query.b)}`);
-})
+router.post('/sub', (req, res, next) => {
+    const a = parseFloat(req.body.num1);
+    const b = parseFloat(req.body.num2);
+    if (isNaN(a) || isNaN(b)) {
+        res.send('Invalid value!');
+    } else {
+        req.result = a - b;
+        next();
+    }
+});
+
+router.post('/sub', (req, res) => {
+    res.render('mathsite', { 
+        title: 'Subtraction',
+        action: '/sub',
+        buttonLabel: 'Subtract Numbers',
+        result: req.result
+    });
+});
+
 
 module.exports = router;
