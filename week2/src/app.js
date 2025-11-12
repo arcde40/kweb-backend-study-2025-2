d60a5fe0ffef83a580740112967981919f302c74
Donghoon <alovmeson@naver.com>
2025-11-12 16:14:26 +0900

    week3 assignment

--- week2/src/app.js
+++ week2/src/app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const userRouter = require('./route/user');
 const mathRouter = require('./route/math');
 
-app.use((req, res, next) => {
-  console.log(`Someone sent a request: ${req.url}`);
-  next();
-});
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/user', userRouter);
-
 app.use('/math', mathRouter);
 
-app.get('/', (req, res) => { res.send("Hello, World!"); });
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Main Page' });
+});
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
