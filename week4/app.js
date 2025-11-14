d7d74c6598ecbe3d58f207fb0133ddb9ef6d6549
seongyeon <dkstjddus313@gmail.com>
2025-11-14 12:37:40 +0900

    week4

--- week4/app.js
+++ week4/app.js
@@ -1,5 +1,12 @@
 const {runQuery} = require('./database');
 
+
+const getStudentCredit = async (student_name) => {
+    const query = `select group_concat(enrollments.grade) from students natural join enrollments where student.student_name = '${student_name}';`
+    const result = await runQuery(query);
+    console.log(result[0].grade);
+};
+
 (async () => {
-    console.log(await runQuery('select * from author;'));
+    getStudentCredit('이순신');
 })();
--- week4/database.js
+++ week4/database.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise');
 
+/*
 const pool = mysql.createPool({
     host: 'db',
     port: 3306,
@@ -7,7 +8,16 @@ const pool = mysql.createPool({
     database: 'kweb_db',
     password: '1q2w3e4r'
 });
+*/
 
+// 로컬 환경
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'kweb',
+    database: 'kweb_db',
+    password: '0313'
+});
 
 const runQuery = async sql => {
     const conn = await pool.getConnection();
