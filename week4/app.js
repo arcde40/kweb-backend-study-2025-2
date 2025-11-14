31d4cc181e40b52ac6b241cc90e8acd0248015f3
seongyeon <dkstjddus313@gmail.com>
2025-11-14 13:09:44 +0900

    week4 hw

--- week4/app.js
+++ week4/app.js
@@ -1,12 +1,18 @@
 const {runQuery} = require('./database');
 
-
-const getStudentCredit = async (student_name) => {
-    const query = `select group_concat(enrollments.grade) from students natural join enrollments where student.student_name = '${student_name}';`
+const getStudentGrade = async (student_name) => {
+    const query = `select group_concat(enrollments.grade) as grades from students natural join enrollments where students.student_name = '${student_name}';`
     const result = await runQuery(query);
-    console.log(result[0].grade);
+    console.log(result[0].grades);
+};
+
+const getCourseStudent = async (course_name) => {
+    const query = `select group_concat(students.student_name) as names from courses natural join enrollments natural join students where courses.course_name = '${course_name}';`
+    const result = await runQuery(query);
+    console.log(result[0].names);
 };
 
 (async () => {
-    getStudentCredit('이순신');
+    getStudentGrade('이순신');
+    getCourseStudent('데이터베이스');
 })();
