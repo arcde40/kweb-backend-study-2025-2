52c134181fa74b2cb48d147bb7e05798e432cdc2
arcde40 <arcde40@gmail.com>
2025-11-19 17:35:08 +0900

    chore: week5 (#72)

---
+++ week5/app.js
@@ -0,0 +1,26 @@
+const {runQuery} = require('./database');
+
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+
+app.use(session({
+	secret: 'asdf',
+	resave: false,
+	saveUninitialized: true,
+});
+
+app.get('/me', (req, res) => {
+
+});
+
+app.get('/login', (req, res) => {
+
+});
+
+app.get('/logout', (req, res) => {
+
+});
+
+app.listen(3000, ()=>console.log('server started!'));
