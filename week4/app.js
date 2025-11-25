9d8dc4f3914991e435f547adbf0120f367bd9a1b
samminmul <74859160+samminmul@users.noreply.github.com>
2025-11-25 13:02:22 +0000

    week5

--- week4/app.js
+++ week4/app.js
@@ -3,14 +3,18 @@ const sanitizeHtml = require('sanitize-html');
 
 
 const getStudentCredit = async (student_name) => {
-    const result = await runQuery('select group_concat(enrollments.grade) as grade from students natural join enrollments where students.student_name = ? ',
-        [student_name])
+    const result = await runQuery(
+        'select group_concat(enrollments.grade) as grade from students natural join enrollments where students.student_name = ? ',
+        [student_name]
+    )
     console.log(result[0].grade)
 }
 
 const getCourseStudents = async (course_name) => {
-    const result = await runQuery('select group_concat(students.student_name) as name from enrollments natural join students natural join courses where courses.course_name = ?',
-        [course_name])
+    const result = await runQuery(
+        'select group_concat(students.student_name) as name from enrollments natural join students natural join courses where courses.course_name = ?',
+        [course_name]
+    )
     console.log(result[0].name)
 }
 
