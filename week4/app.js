37bd0c9a0ab8d85370a43201272c164009a49307
samminmul <74859160+samminmul@users.noreply.github.com>
2025-11-25 12:50:17 +0000

    feat: add package.json and package-lock.json for dependency management

--- week4/app.js
+++ week4/app.js
@@ -1,5 +1,28 @@
 const {runQuery} = require('./database');
+const sanitizeHtml = require('sanitize-html');
+
+
+const getStudentCredit = async (student_name) => {
+    const result = await runQuery('select group_concat(enrollments.grade) as grade from students natural join enrollments where students.student_name = ? ',
+        [student_name])
+    console.log(result[0].grade)
+}
+
+const getCourseStudents = async (course_name) => {
+    const result = await runQuery('select group_concat(students.student_name) as name from enrollments natural join students natural join courses where courses.course_name = ?',
+        [course_name])
+    console.log(result[0].name)
+}
+
+const insertStudentCourse = async (student_id, course_id, grade) => {
+    await runQuery(
+        'insert into enrollments (student_id, course_id, grade) values (?, ?, ?) ON DUPLICATE KEY UPDATE grade = VALUES(grade)',
+        [student_id, course_id, grade]
+    )
+}
 
 (async () => {
-    console.log(await runQuery('select * from author;'));
+    await getStudentCredit('이순신')
+    await getCourseStudents('자료구조')
+    await insertStudentCourse(1, 2, 'A0')
 })();
