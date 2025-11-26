f8b9793fdfc44c08db8162d393ce9072dc321dd3
nickgave <61822252+nickgave@users.noreply.github.com>
2025-11-26 04:28:35 +0000

    Assignment for Week 5

---
+++ week5/assignment/database.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: 'db',
+    port: 3306,
+    user: 'kweb',
+    database: 'kweb_db',
+    password: '1q2w3e4r'
+});
+
+
+const runQuery = async (pstmt, data) => {
+    const conn = await pool.getConnection();
+    try {
+        const sql = conn.format(pstmt, data);
+        const [result] = await conn.query(sql);
+        return result;
+    } finally {
+        conn.release();
+    }
+};
+
+module.exports = {runQuery}
